332f188a45cb5f1af8616c5df1119dfc8a26a051
Christopher Simpson <Christopherleesimpson1@gmail.com>
2021-04-18 11:06:22 -0400

    Initial commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<!--Author: Christopher Lee Simpson
+	Date:  Sunday, April 18, 2021
+		  
+-->
+
+<html>
+<head>
+	<title>index.php</title>
+	
+</head>
+<body>
+	<h1>index.php</h1>
+
+	<a href="index.php">Link</a>
+</body>
+</html>
