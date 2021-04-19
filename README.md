a8f818b09d465e7f5a84a19ce4db3d92931119ff
Christopher Simpson <Christopherleesimpson1@gmail.com>
2021-04-19 16:38:21 -0400

    After altering the index.php page

---
+++ README.md
@@ -0,0 +1,3 @@
+#asgn10 - names
+
+This program is an exercise for school to pratice PHP concepts: arrays, functions, and
--- index.php
+++ index.php
@@ -1,8 +1,4 @@
 <!DOCTYPE html>
-<!--Author: Christopher Lee Simpson
-	Date:  Sunday, April 18, 2021
-		  
--->
 
 <html>
 <head>
@@ -10,8 +6,128 @@
 	
 </head>
 <body>
-	<h1>index.php</h1>
+	<h1>Names</h1>
 
 	<a href="index.php">Link</a>
+<?php
+
+$fileName = "names-short-list.txt";
+
+$lineNumber = 0;
+
+
+// load the array
+$FH = fopen("$fileName", "r");
+$nextName = fgets($FH);
+
+while(!feof($FH)) {
+
+if ($lineNumber % 2 == 0) {
+
+$fullNames[] = trim(substr($nextName, 0, strpos($nextName, " ==")));
+
+}
+
+$lineNumber++;
+$nextName = fgets($FH);
+
+
+
+}
+
+// trim the names
+foreach($fullNames as $fullName) {
+     $startHere = strpos($fullName, ",") + 1;
+	 $firstNames[] = trim(substr($fullName, $startHere));
+	 
+
+}
+
+foreach($fullNames as $fullName) {
+     $stopHere = strpos($fullName, ",");
+	 $lastNames[] = substr($fullName, 0, $stopHere);
+
+}
+
+
+// get valid names
+for ($i = 0; $i < sizeof($fullNames); $i++) {
+     if (ctype_alpha($lastNames[$i].$firstNames[$i])) {
+          
+		$validFirstNames[$i] = $firstNames[$i];
+		$validLastNames[$i] = $lastNames[$i];
+		$validFullNames[$i] = $validLastNames[$i] . ", " . $validFirstNames[$i];
+
+	 }
+
+}
+
+
+// display results
+
+echo"<h1>Names - Results</h1>";
+
+echo"<h2>All Names</h2>";
+
+echo"There are " . sizeof($fullNames) . " total names.";
+
+foreach($fullNames as $fullName) {
+
+	echo"<p>$fullName</p>< /br>";
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 </body>
 </html>
